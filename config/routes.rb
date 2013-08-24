a75d632f09bc9c6f370c1486200234b2c9b8245f
Mattan Griffel <mattan@onemonthrails.com>
2013-08-24 13:55:01 -0700

    Creating home and about pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Pinteresting::Application.routes.draw do
+  root "pages#home"
+  get "about" => "pages#about" # creates about_path
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
